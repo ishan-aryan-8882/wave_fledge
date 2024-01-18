0b246b15e30d69b1bd8543b2835f868e46657910
unknown <Devl-Minhaj>
2024-01-18 18:18:14 +0530

    file save

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,22 @@ $(document).ready(function () {
   //------------end----------------
 
   //mode toggle
-  let darkToggle= $('.li')
+  let darkToggle = $("img.light-page");
+  let lightToggle = $("img.dark-page");
+
   $(".dark-light-mode img").on("click", function () {
     $(".dark-light-mode img").not($(this)).removeClass("dn-class");
     $(this).addClass("dn-class");
-  });
-  if('img.dark-page'){
 
-  }
+    // Check if the clicked image has the class 'light-page'
+    if ($(this).hasClass("light-page")) {
+      $("body").addClass("light-theme-page");
+      $("body").removeClass("dark-theme-page");
+    } else {
+      $("body").addClass("dark-theme-page");
+      $("body").removeClass("light-theme-page");
+    }
+  });
+
+  //------------end----------------
 });
